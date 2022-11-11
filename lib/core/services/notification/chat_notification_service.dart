ab8a276c357d77ffc9c9fa741e91bcb4630795e3
Adrianzctpa <nocommentaryadrian@gmail.com>
2022-11-11 14:43:37 -0300

    finished notifications

--- lib/core/services/notification/chat_notification_service.dart
+++ lib/core/services/notification/chat_notification_service.dart
@@ -1,8 +1,9 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat/core/models/chat_notification.dart';
 
 class ChatNotificationService with ChangeNotifier {
-  List<ChatNotification> _items = [];
+  final List<ChatNotification> _items = [];
 
   List<ChatNotification> get items => _items;
 
@@ -18,5 +19,49 @@ class ChatNotificationService with ChangeNotifier {
   void remove(int i) {
     _items.removeAt(i);
     notifyListeners();
+  }
+
+  Future<void> init() async {
+    await _configureForeground();
+    await _configureBackground();
+    await _configureExited();
+  }
+
+  Future<void> _configureForeground() async {
+    if (await _requestPermissions()) {
+      FirebaseMessaging.onMessage.listen(_messageHandler);
+    }
+  }
+
+  Future<void> _configureBackground() async {
+    if (await _requestPermissions()) {
+      FirebaseMessaging.onMessageOpenedApp.listen(_messageHandler);
+    }
+  }
+
+  Future<void> _configureExited() async {
+    if (await _requestPermissions()) {
+      RemoteMessage? initialNtf = await FirebaseMessaging.instance.getInitialMessage();
+
+      if (initialNtf == null) return;
+
+      _messageHandler(initialNtf);
+    }
+  }
+
+  void _messageHandler(RemoteMessage message) {
+    final notification = message.notification;
+    if (notification == null) return;
+    
+    add(ChatNotification(
+      title: notification.title ?? 'No title',
+      body: notification.body ?? 'None',
+    )); 
+  }
+
+  Future<bool> _requestPermissions() async {
+    final messaging = FirebaseMessaging.instance;
+    final settings = await messaging.requestPermission();
+    return settings.authorizationStatus == AuthorizationStatus.authorized;
   }
 }
--- lib/pages/auth_app_swapper.dart
+++ lib/pages/auth_app_swapper.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chat/core/models/chat_user.dart';
 import 'package:flutter_chat/core/services/auth/auth_service.dart';
+import 'package:flutter_chat/core/services/notification/chat_notification_service.dart';
 import 'package:flutter_chat/pages/auth_page.dart';  
 import 'package:flutter_chat/pages/chat_page.dart';  
 import 'package:flutter_chat/pages/loading_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
 class AuthAppSwapper extends StatelessWidget {
   const AuthAppSwapper({Key? key}) : super(key: key);
 
-  Future<void> init(BuildContext context) async {
+  Future<void> init(BuildContext context, [bool mounted = true]) async {    
     await Firebase.initializeApp();
+    if (!mounted) return;
+    await Provider.of<ChatNotificationService>(context, listen: false).init();
   }
 
   @override
